ffc64f0fef2f74df24df7d062045f4ce81a63f0a
MEDIVH <now1124@hotmail.com>
2024-01-17 18:57:20 +0700

    test

--- merge.ts
+++ merge.ts
@@ -25,10 +25,19 @@ function mergeArrays(collection1: number[], collection2: number[]): number[] {
 
   return mergedArray;
 }
-
-// Example usage:
+// TEST CASE
 const collection_1 = [1, 3, 5, 7];
 const collection_2 = [2, 4, 6, 8, 10];
 
-const mergedArray = mergeArrays(collection_1, collection_2);
-console.log(mergedArray);
+const mergedArray1 = mergeArrays(collection_1, collection_2);
+
+console.log("case 1: " + mergedArray1);
+
+console.log(
+  "============================================================================"
+);
+const collection_3 = [1, 1, 1, 1, 9, 9, 9, 10, 11];
+const collection_4 = [0, 1, 6, 8, 10];
+
+const mergedArray2 = mergeArrays(collection_3, collection_4);
+console.log("case 2: " + mergedArray2);
