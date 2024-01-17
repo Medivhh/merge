379bfe86624bbd2c2f17c0e56f7012411ae5f15c
MEDIVH <now1124@hotmail.com>
2024-01-17 19:45:35 +0700

    test unit

---
+++ merge.test.ts
@@ -0,0 +1,27 @@
+import merge from "./merge";
+
+describe("merge arrays function", () => {
+  it("merge 2 sorted arrays", () => {
+    const array1 = [1, 3, 5, 7, 9];
+    const array2 = [2, 4, 6, 8, 10];
+    const result = merge(array1, array2);
+
+    expect(result).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  it("merge with empty arrays", () => {
+    const array1: number[] = [];
+    const array2 = [2, 4, 6, 8, 10];
+    const result = merge(array1, array2);
+
+    expect(result).toEqual([2, 4, 6, 8, 10]);
+  });
+
+  it("merge 2 empty array ", () => {
+    const array1: number[] = [];
+    const array2: number[] = [];
+    const result = merge(array1, array2);
+
+    expect(result).toEqual([]);
+  });
+});
--- merge.ts
+++ merge.ts
@@ -1,4 +1,7 @@
-function mergeArrays(collection1: number[], collection2: number[]): number[] {
+export default function mergeArrays(
+  collection1: number[],
+  collection2: number[]
+): number[] {
   const mergedArray: number[] = [];
   let i = 0;
   let j = 0;
@@ -25,19 +28,3 @@ function mergeArrays(collection1: number[], collection2: number[]): number[] {
 
   return mergedArray;
 }
-// TEST CASE
-const collection_1 = [1, 3, 5, 7];
-const collection_2 = [2, 4, 6, 8, 10];
-
-const mergedArray1 = mergeArrays(collection_1, collection_2);
-
-console.log("case 1: " + mergedArray1);
-
-console.log(
-  "============================================================================"
-);
-const collection_3 = [1, 1, 1, 1, 9, 9, 9, 10, 11];
-const collection_4 = [0, 1, 6, 8, 10];
-
-const mergedArray2 = mergeArrays(collection_3, collection_4);
-console.log("case 2: " + mergedArray2);
